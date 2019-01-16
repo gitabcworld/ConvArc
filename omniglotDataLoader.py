Fixed mean-std calculation for omniglot. Done visualization of the glimpses.

omniglotDataLoader.py
@@ -9,6 +9,7 @@ import math
 import psutil
 import gc
 
+import torch
 from util import cvtransforms as T
 import torchvision.transforms as transforms
 
@@ -39,19 +40,6 @@ BanknoteTripletsROI: Triplets of ROIs banknotes. The ROIs are already cropped at
 
 class omniglotDataLoader():
 
-    class windows_lambda():
-        def __init__(self,opt):
-            self.opt = opt
-        def cv2_scale(self,x):
-            return cv2.resize(x, dsize=(self.opt.imageSize, self.opt.imageSize),
-                                        interpolation=cv2.INTER_AREA).astype(np.uint8)
-        def np_reshape(self,x):
-            return np.reshape(x, (self.opt.imageSize, self.opt.imageSize, 1))
-        def np_repeat(self,x):
-            return np.repeat(x, self.opt.nchannels, axis=2)
-        def np_mul(self,x):
-            return lambda x: x * 255.0
-
     def __init__(self,type=Omniglot, opt=Options().parse(), train_mean=None, train_std=None):
         self.type = type
         self.opt = opt
@@ -59,53 +47,31 @@ class omniglotDataLoader():
             self.train_mean = None
             self.train_std = None
         else:
-            #self.train_mean = torch.from_numpy(train_mean)
-            #self.train_std = torch.from_numpy(train_std)
             self.train_mean = train_mean
             self.train_std = train_std
 
     def __get_mean_std__(self):
 
         kwargs = {'num_workers': self.opt.nthread, 'pin_memory': True} if self.opt.cuda else {}
-        # WINDOWS - LINUX. Lambda objects cannot be pickled on Windows. Thread: https://discuss.pytorch.org/t/cant-pickle-local-object-dataloader-init-locals-lambda/31857
-        if os.name == 'nt': # Windows
-            
-            obj_windows_lambda = self.windows_lambda(opt=self.opt)
+        cv2_scale = lambda x: cv2.resize(x, dsize=(self.opt.imageSize, self.opt.imageSize),
+                                        interpolation=cv2.INTER_AREA).astype(np.uint8)
+        np_reshape = lambda x: np.reshape(x, (self.opt.imageSize, self.opt.imageSize, 1))
+        np_repeat = lambda x: np.repeat(x, self.opt.nchannels, axis=2)
+        np_mul = lambda x: (x * 255.0).astype(np.uint8)
 
-            train_transform = transforms.Compose([
-                #obj_windows_lambda.cv2_scale,
-                obj_windows_lambda.np_reshape,
-                obj_windows_lambda.np_repeat,
-                #T.AugmentationAleju(channel_is_first_axis=False,
-                #                    scale_to_percent=self.opt.scale,
-                #                    hflip=self.opt.hflip, vflip=self.opt.vflip,
-                #                    rotation_deg=self.opt.rotation_deg, shear_deg=self.opt.shear_deg,
-                #                    translation_x_px=self.opt.translation_px,
-                #                    translation_y_px=self.opt.translation_px),
-                #obj_windows_lambda.np_mul,
-                #transforms.ToTensor(),
-            ])
-
-        else:
-            cv2_scale = lambda x: cv2.resize(x, dsize=(self.opt.imageSize, self.opt.imageSize),
-                                            interpolation=cv2.INTER_AREA).astype(np.uint8)
-            np_reshape = lambda x: np.reshape(x, (self.opt.imageSize, self.opt.imageSize, 1))
-            np_repeat = lambda x: np.repeat(x, self.opt.nchannels, axis=2)
-            np_mul = lambda x: x * 255.0
-
-            train_transform = transforms.Compose([
-                #cv2_scale,
-                np_reshape,
-                np_repeat,
-                #T.AugmentationAleju(channel_is_first_axis=False,
-                #                    scale_to_percent=self.opt.scale,
-                #                    hflip=self.opt.hflip, vflip=self.opt.vflip,
-                #                    rotation_deg=self.opt.rotation_deg, shear_deg=self.opt.shear_deg,
-                #                    translation_x_px=self.opt.translation_px,
-                #                    translation_y_px=self.opt.translation_px),
-                #np_mul,
-                #transforms.ToTensor(),
-            ])
+        train_transform = transforms.Compose([
+            #cv2_scale,
+            np_reshape,
+            np_repeat,
+            T.AugmentationAleju(channel_is_first_axis=False,
+                                scale_to_percent=self.opt.scale,
+                                hflip=self.opt.hflip, vflip=self.opt.vflip,
+                                rotation_deg=self.opt.rotation_deg, shear_deg=self.opt.shear_deg,
+                                translation_x_px=self.opt.translation_px,
+                                translation_y_px=self.opt.translation_px),
+            np_mul,
+            transforms.ToTensor(),
+        ])
 
         train_loader_mean_std = torch.utils.data.DataLoader(
             Omniglot(root=self.opt.dataroot,
@@ -117,7 +83,7 @@ class omniglotDataLoader():
         pbar = tqdm(enumerate(train_loader_mean_std))
         tmp = []
         for batch_idx, (data, labels) in pbar:
-            tmp.append(data)
+            tmp.append(data.data.cpu().numpy())
             pbar.set_description(
                 '[{}/{} ({:.0f}%)]\t'.format(
                     batch_idx * len(data), len(train_loader_mean_std.dataset),
@@ -128,13 +94,9 @@ class omniglotDataLoader():
             if int(math.floor(free_mem)) == 0:
                 break
 
-        #train_mean = torch.cat(tmp).mean()
-        #train_std = torch.cat(tmp).std()
-
         tmp = np.vstack(tmp)
         train_mean = tmp.mean(axis=0)
-        #train_std = tmp.std(axis=0)
-        train_std = np.ones(train_mean.shape)
+        train_std = tmp.std(axis=0)
 
         # Free memory
         tmp = []
@@ -159,7 +121,7 @@ class omniglotDataLoader():
                                          interpolation=cv2.INTER_AREA).astype(np.uint8)
         np_reshape = lambda x: np.reshape(x, (self.opt.imageSize, self.opt.imageSize, 1))
         np_repeat = lambda x: np.repeat(x, self.opt.nchannels, axis=2)
-        np_mul = lambda x: x*255.0
+        np_mul = lambda x: (x*255.0).astype(np.uint8)
 
         #################################
         # TRANSFORMATIONS: transformations for the TRAIN dataset
@@ -176,9 +138,9 @@ class omniglotDataLoader():
                                 translation_y_px=self.opt.translation_px),
             np_mul,
             #T.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-            T.Normalize(self.train_mean, self.train_std),
-            transforms.ToTensor(),
             #T.Normalize(self.train_mean, self.train_std),
+            transforms.ToTensor(),
+            T.Normalize(torch.from_numpy(self.train_mean).float(), torch.from_numpy(self.train_std).float()),
             #T.Normalize(train_mean, train_std),
         ])
 
@@ -208,9 +170,9 @@ class omniglotDataLoader():
             np_reshape,
             np_repeat,
             #T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            T.Normalize(self.train_mean, self.train_std),
+            #T.Normalize(self.train_mean, self.train_std),
             transforms.ToTensor(),
-            #T.Normalize(train_mean, train_std),
+            T.Normalize(torch.from_numpy(self.train_mean).float(), torch.from_numpy(self.train_std).float()),
         ])
 
         if self.type == OmniglotOneShot:

viz.py
@@ -1,36 +1,75 @@
-import argparse
-import os
-
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import shutil
 
+import os
+import sys
+
+# Add all the python paths needed to execute when using Python 3.6
+sys.path.append(os.path.join(os.path.dirname(__file__), "models"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "models/arc"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "skiprnn_pytorch"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "models/wrn"))
+
+import time
 import numpy as np
+from datetime import datetime, timedelta
 from logger import Logger
 import torch
-from torch.autograd import Variable
-import torch.backends.cudnn as cudnn
+import torch.nn as nn
+from sklearn.metrics import accuracy_score
 import shutil
+
 from models import models
 from models.models import ArcBinaryClassifier
 
-from omniglotBenchmarks import omniglotBenchMark
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
 
 from option import Options, tranform_options
-from dataset.omniglot_pytorch import OmniglotOSPairs
-from dataset.omniglot_pytorch import OmniglotOneShot
+
+# Omniglot dataset
+from omniglotDataLoader import omniglotDataLoader
+from dataset.omniglot import Omniglot
+from dataset.omniglot import Omniglot_Pairs
+from dataset.omniglot import OmniglotOneShot
+
+# Mini-imagenet dataset
+from miniimagenetDataLoader import miniImagenetDataLoader
+from dataset.mini_imagenet import MiniImagenet
+from dataset.mini_imagenet import MiniImagenetPairs
+from dataset.mini_imagenet import MiniImagenetOneShot
+
+# Banknote dataset
+from dataset.banknote_pytorch import FullBanknoteROI
+
 from models.conv_cnn import ConvCNNFactory
-from do_epoch_fns import do_epoch_ARC, do_epoch_ARC_unroll
+from models.fullContext import FullContextARC
+from models.naiveARC import NaiveARC
+
+from do_epoch_fns import do_epoch_ARC, do_epoch_ARC_unroll, do_epoch_naive_full
 import arc_train
 import arc_val
 import arc_test
+import context_train
+import context_val
+import context_test
 
-opt = parser.parse_args()
+import multiprocessing
 
-def display(image1, mask1, image2, mask2, name="hola.png"):
+import cv2
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+# CUDA_VISIBLE_DEVICES == 1 (710) / CUDA_VISIBLE_DEVICES == 0 (1070)
+#import os
+#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+def display(opt, image1, mask1, image2, mask2, name="no_name.png"):
     _, ax = plt.subplots(1, 2)
 
     # a heuristic for deciding cutoff
-    masking_cutoff = 2.4 / (opt.glimpseSize)**2
+    masking_cutoff = 2.4 / (opt.arc_glimpseSize)**2
 
     mask1 = (mask1 > masking_cutoff).data.numpy()
     mask1 = np.ma.masked_where(mask1 == 0, mask1)
@@ -38,81 +77,24 @@ def display(image1, mask1, image2, mask2, name="hola.png"):
     mask2 = (mask2 > masking_cutoff).data.numpy()
     mask2 = np.ma.masked_where(mask2 == 0, mask2)
 
-    ax[0].imshow(image1.data.numpy()/255, cmap=mpl.cm.bone)
+    ax[0].imshow(image1, cmap=mpl.cm.bone)
     ax[0].imshow(mask1, interpolation="nearest", cmap=mpl.cm.jet_r, alpha=0.7)
 
-    ax[1].imshow(image2.data.numpy()/255, cmap=mpl.cm.bone)
+    ax[1].imshow(image2, cmap=mpl.cm.bone)
     ax[1].imshow(mask2, interpolation="nearest", cmap=mpl.cm.ocean, alpha=0.7)
 
-    plt.savefig(os.path.join(images_path, name))
+    plt.savefig(os.path.join('D:/PhD/logs/images/', name))
 
 
-def get_sample(discriminator):
-
-    # size of the set to choose sample from from
-    sample_size = 30
-    X, Y = batcher.fetch_batch("train", batch_size=sample_size)
-    pred = discriminator(X)
-
-    if opt.same:
-        same_pred = pred[sample_size // 2:].data.numpy()[:, 0]
-        mx = same_pred.argsort()[len(same_pred) // 2]  # choose the sample with median confidence
-        index = mx + sample_size // 2
-    else:
-        diff_pred = pred[:sample_size // 2].data.numpy()[:, 0]
-        mx = diff_pred.argsort()[len(diff_pred) // 2]  # choose the sample with median confidence
-        index = mx
-
-    return X[index]
-
-
-def visualize():
-
-    # initialise the model
-    discriminator = ArcBinaryClassifier(num_glimpses=opt.numGlimpses,
-                                        glimpse_h=opt.glimpseSize,
-                                        glimpse_w=opt.glimpseSize,
-                                        controller_out=opt.numStates)
-    discriminator.load_state_dict(torch.load(os.path.join("saved_models", opt.name, opt.load)))
-
-    arc = discriminator.arc
-
-    sample = get_sample(discriminator)
-
-    all_hidden = arc._forward(sample[None, :, :])[:, 0, :]  # (2*numGlimpses, controller_out)
-    glimpse_params = torch.tanh(arc.glimpser(all_hidden))
-    _, channels, height, witdth = sample.shape
-    masks = arc.glimpse_window.get_attention_mask(glimpse_params, mask_h=height, mask_w=witdth)
-
-    # separate the masks of each image.
-    masks1 = []
-    masks2 = []
-    for i, mask in enumerate(masks):
-        if i % 2 == 1:  # the first image outputs the hidden state for the next image
-            masks1.append(mask)
-        else:
-            masks2.append(mask)
-
-    channels = 3
-    for i, (mask1, mask2) in enumerate(zip(masks1, masks2)):
-        sample_0 = (sample[0].transpose(0, 1).transpose(1, 2) + \
-                    Variable(torch.from_numpy(batcher.mean_pixel)).float()) * 255
-        sample_1 = (sample[1].transpose(0, 1).transpose(1, 2) + \
-                    Variable(torch.from_numpy(batcher.mean_pixel)).float()) * 255
-
-        display(sample_0, mask1, sample_1, mask2,"img_{}".format(i))
-
-
-if __name__ == "__main__":
-    visualize()
-
-
-
-def train(index = 0):
+def visualize(index = 0):
 
     # change parameters
     options = Options().parse()
+    #options = Options().parse() if options is None else options
     options = tranform_options(index, options)
+    # use cuda?
+    options.cuda = torch.cuda.is_available()
+
     cudnn.benchmark = True # set True to speedup
 
     train_mean = None
@@ -120,13 +102,30 @@ def train(index = 0):
     if os.path.exists(os.path.join(options.save, 'mean.npy')):
         train_mean = np.load(os.path.join(options.save, 'mean.npy'))
         train_std = np.load(os.path.join(options.save, 'std.npy'))
-    bnktBenchmark = omniglotBenchMark(type=OmniglotOSPairs, opt=options, train_mean=train_mean,
-                                      train_std=train_std)
-    opt = bnktBenchmark.opt
-    train_loader, val_loader, test_loader = bnktBenchmark.get()
+    
+    if options.datasetName == 'miniImagenet':
+        dataLoader = miniImagenetDataLoader(type=MiniImagenetPairs, opt=options)
+    elif options.datasetName == 'omniglot':
+        dataLoader = omniglotDataLoader(type=Omniglot_Pairs, opt=options, train_mean=train_mean,
+                                        train_std=train_std)
+    else:
+        pass
 
-    train_mean = bnktBenchmark.train_mean
-    train_std = bnktBenchmark.train_std
+    # Get the params
+    opt = dataLoader.opt
+    
+    # Use the same seed to split the train - val - test
+    if os.path.exists(os.path.join(options.save, 'dataloader_rnd_seed_arc.npy')):
+        rnd_seed = np.load(os.path.join(options.save, 'dataloader_rnd_seed_arc.npy'))
+    else:    
+        rnd_seed = np.random.randint(0, 100000)
+        np.save(os.path.join(opt.save, 'dataloader_rnd_seed_arc.npy'), rnd_seed)
+
+    # Get the DataLoaders from train - val - test
+    train_loader, val_loader, test_loader = dataLoader.get(rnd_seed=rnd_seed)
+
+    train_mean = dataLoader.train_mean
+    train_std = dataLoader.train_std
     if not os.path.exists(os.path.join(options.save, 'mean.npy')):
         np.save(os.path.join(opt.save, 'mean.npy'), train_mean)
         np.save(os.path.join(opt.save, 'std.npy'), train_std)
@@ -144,10 +143,15 @@ def train(index = 0):
                                         opt.arc_glimpseSize, opt.arc_numStates,
                                         "cuda" if opt.cuda else "cpu")
 
-    # make directory for storing images.
-    images_path = os.path.join(opt.name, "visualization")
-    if not os.path.isdir(images_path):
-        os.makedirs(images_path)
+    print("[{}]. Will start training {} with parameters:\n{}\n\n".format(multiprocessing.current_process().name,
+                                                                         opt.name, opt))
+
+    # make directory for storing models.
+    models_path = os.path.join(opt.save, opt.name)
+    if not os.path.isdir(models_path):
+	    os.makedirs(models_path)
+    else:
+        shutil.rmtree(models_path)
 
     fcn = None
     convCNN = None
@@ -172,29 +176,57 @@ def train(index = 0):
                                         attn_dense=opt.arc_attn_dense)
 
     # load from a previous checkpoint, if specified.
-    if opt.arc_load is not None:
+    if opt.arc_load is not None and os.path.exists(opt.arc_load):
         discriminator.load_state_dict(torch.load(opt.arc_load))
 
     if opt.cuda:
         discriminator.cuda()
 
-    # Select the epoch functions
-    do_epoch_fn = None
-    if opt.arc_attn_unroll == True:
-        do_epoch_fn = do_epoch_ARC_unroll
-    else:
-        do_epoch_fn = do_epoch_ARC
+    # Set for the first batch a random seed for AumentationAleju
+    train_loader.dataset.agumentation_seed = int(np.random.rand() * 1000)
 
-    ###################################
-    ## TRAINING ARC/CONVARC
-    ###################################
-    epoch = 0
-    logger = None
-    optimizer = None
-    loss_fn = None
-    val_acc_epoch, val_loss_epoch, is_model_saved = arc_val.arc_val(epoch, do_epoch_fn, opt, val_loader,
-                                                                    discriminator, logger,
-                                                                    convCNN=convCNN,
-                                                                    optimizer=optimizer,
-                                                                    loss_fn=loss_fn, fcn=fcn)
+    for batch_idx, (data, label) in enumerate(train_loader):
 
+        if opt.cuda:
+            data = data.cuda()
+            label = label.cuda()
+        inputs = Variable(data, requires_grad=False)
+        targets = Variable(label)
+
+        batch_size, npair, nchannels, x_size, y_size = inputs.shape
+        inputs = inputs.view(batch_size * npair, nchannels, x_size, y_size)
+        if fcn:
+            inputs = fcn(inputs)
+        _ , nfilters, featx_size, featy_size = inputs.shape
+        inputs = inputs.view(batch_size, npair, nfilters, featx_size, featy_size)
+
+        #features, updated_states = discriminator(inputs)
+        all_hidden = discriminator.arc._forward(inputs)
+        glimpse_params = torch.tanh(discriminator.arc.glimpser(all_hidden)) # return [num_glimpses*2,batchsize,(x, y, delta)]
+        
+        sample = data[0]
+        _, channels, height, witdth = sample.shape
+
+        # separate the masks of each image.
+        masks1 = []
+        masks2 = []
+        for i in range(glimpse_params.shape[0]):
+            mask = discriminator.arc.glimpse_window.get_attention_mask(glimpse_params[i], mask_h=height, mask_w=witdth)
+            if i % 2 == 1:  # the first image outputs the hidden state for the next image
+                masks1.append(mask)
+            else:
+                masks2.append(mask)
+
+        channels = 3
+        for glimpse_i, (mask1, mask2) in enumerate(zip(masks1, masks2)):
+            for batch_i in range(data.shape[0]):
+                sample_0 = ((data[batch_i,0].data.cpu().numpy() * train_std + train_mean)*255.0).transpose(1,2,0).astype(np.uint8)
+                sample_1 = ((data[batch_i,1].data.cpu().numpy() * train_std + train_mean)*255.0).transpose(1,2,0).astype(np.uint8)
+                if sample_0.shape[2] == 1:
+                    sample_0 = np.repeat(sample_0,3,axis=2)
+                    sample_1 = np.repeat(sample_1,3,axis=2)
+                display(opt, sample_0, mask1[batch_i], sample_1, mask2[batch_i],"img_batch_%d_glimpse_%d.png" % (batch_i,glimpse_i))
+
+
+if __name__ == "__main__":
+    visualize()