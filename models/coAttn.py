a44ade603b41cf8383b2a2291a8b95477022d099
MITEK\aberenguel <aberenguel@miteksystems.com>
2019-02-07 21:04:20 +0100

    Changed co-attention matrix to calculate mean along the channels

--- models/coAttn.py
+++ models/coAttn.py
@@ -39,19 +39,23 @@ class CoAttn(nn.Module):
             Q_a_1 = Q_a_1.view(batch_size*nfilters,-1).unsqueeze(2).expand(batch_size*nfilters,self.size_pow[0],self.size_pow[1]) # 20480 x 49 x 49
             Q_b_1 = Q_b_1.transpose(1,2).contiguous().view(batch_size*nfilters,-1).unsqueeze(2).expand(batch_size*nfilters,self.size_pow[0],self.size_pow[1]) # 20480 x 49 x 49
             L = Q_b_1*self.W*Q_a_1 # 20480 x 49 x 49
-            A_a = torch.nn.Softmax(dim=1)(L.view(batch_size*nfilters,self.size_pow[0]*self.size_pow[1])).view(batch_size*nfilters,self.size_pow[0],self.size_pow[1]) # 20480 x 49 x 49
-            A_b = torch.nn.Softmax(dim=1)(L.contiguous().transpose(1,2).contiguous().view(batch_size*nfilters,self.size_pow[0]*self.size_pow[1])).view(batch_size*nfilters,self.size_pow[0],self.size_pow[1]) # 20480 x 49 x 49
-            Z_a = Q_a * A_a.mean(dim=1).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
-            Z_b = Q_b * A_b.mean(dim=1).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
+            #A_a = torch.nn.Softmax(dim=1)(L.view(batch_size*nfilters,self.size_pow[0]*self.size_pow[1])).view(batch_size*nfilters,self.size_pow[0],self.size_pow[1]) # 20480 x 49 x 49
+            A_a = torch.nn.Softmax(dim=3)(L.view(batch_size,nfilters,self.size_pow[0],self.size_pow[1]))
+            #A_b = torch.nn.Softmax(dim=1)(L.contiguous().transpose(1,2).contiguous().view(batch_size*nfilters,self.size_pow[0]*self.size_pow[1])).view(batch_size*nfilters,self.size_pow[0],self.size_pow[1]) # 20480 x 49 x 49
+            A_b = torch.nn.Softmax(dim=3)(L.view(batch_size,nfilters,self.size_pow[0],self.size_pow[1]).transpose(3,2))
+            #Z_a = Q_a * A_a.mean(dim=1).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
+            #Z_b = Q_b * A_b.mean(dim=1).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
+            Z_a = Q_a * A_a.mean(dim=2).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
+            Z_b = Q_b * A_b.mean(dim=2).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
         else:
             ## Activation-Based attention transfer. Paper: Paying More Attention to Attention: Improving the Performance of Convolutional
             ## Neural Networks via Attention Transfer. ICLR 2017. 
             if self.typeActivation == 'sum_abs':
-                Q_a_attn = Q_a.abs().sum(dim=1) # 10 x 7 x 7
-                Q_b_attn = Q_b.abs().sum(dim=1) # 10 x 7 x 7
+                Q_a_attn = Q_a.abs().mean(dim=1) # 10 x 7 x 7
+                Q_b_attn = Q_b.abs().mean(dim=1) # 10 x 7 x 7
             elif self.typeActivation == 'sum_abs_pow': 
-                Q_a_attn = Q_a.abs().pow(self.p).sum(dim=1) # 10 x 7 x 7
-                Q_b_attn = Q_b.abs().pow(self.p).sum(dim=1) # 10 x 7 x 7
+                Q_a_attn = Q_a.abs().pow(self.p).mean(dim=1) # 10 x 7 x 7
+                Q_b_attn = Q_b.abs().pow(self.p).mean(dim=1) # 10 x 7 x 7
             elif self.typeActivation == 'max_abs_pow':
                 Q_a_attn = Q_a.abs().pow(self.p).max(dim=1)[0] # 10 x 7 x 7
                 Q_b_attn = Q_b.abs().pow(self.p).max(dim=1)[0] # 10 x 7 x 7
@@ -59,8 +63,10 @@ class CoAttn(nn.Module):
             Q_a_attn = Q_a_attn.view(batch_size,-1).unsqueeze(2).expand(batch_size,self.size_pow[0],self.size_pow[1]) # 10 x 49 x 49
             Q_b_attn = Q_b_attn.view(batch_size,-1).unsqueeze(2).expand(batch_size,self.size_pow[0],self.size_pow[1]) # 10 x 49 x 49
             L = Q_a_attn.transpose(1,2)*self.W*Q_b_attn # 10 x 49 x 49
-            A_a = torch.nn.Softmax(dim=1)(L.view(-1,self.size_pow[0]*self.size_pow[1])).view(-1,self.size_pow[0],self.size_pow[1]) # 10 x 49 x 49
-            A_b = torch.nn.Softmax(dim=1)(L.transpose(1,2).contiguous().view(-1,self.size_pow[0]*self.size_pow[1])).view(-1,self.size_pow[0],self.size_pow[1]) # 10 x 49 x 49
+            #A_a = torch.nn.Softmax(dim=1)(L.view(-1,self.size_pow[0]*self.size_pow[1])).view(-1,self.size_pow[0],self.size_pow[1]) # 10 x 49 x 49
+            A_a = torch.nn.Softmax(dim=2)(L)
+            #A_b = torch.nn.Softmax(dim=1)(L.transpose(1,2).contiguous().view(-1,self.size_pow[0]*self.size_pow[1])).view(-1,self.size_pow[0],self.size_pow[1]) # 10 x 49 x 49
+            A_b = torch.nn.Softmax(dim=2)(L.transpose(1,2))
             Z_a = torch.bmm(Q_a.view(batch_size,nfilters,-1),A_a).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
             Z_b = torch.bmm(Q_b.view(batch_size,nfilters,-1),A_b).view(batch_size,nfilters,self.size[0],self.size[1]) # 10 x 2048 x 7 x 7
         x = torch.cat((Z_a.unsqueeze(1),Z_b.unsqueeze(1)),1)
